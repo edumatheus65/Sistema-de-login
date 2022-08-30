c5056d97465312a9709a9c33f3e81b650d621fa5
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-30 09:31:48 -0300

    Criado arq style.css para estilizacao do formulario

--- login.php
+++ login.php
@@ -12,21 +12,29 @@
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous"/>
-    
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
     
-    <form action="" method="get">
-        <div class="form-group">
-            <label for="email">Digite seu email</label>
-            <input type="text" class="form-control" id="email" name="email" placeholder="Digite seu email">
-        </div>
-        <div class="form-group">
-            <label for="email">Digite sua senha</label>
-            <input type="password" class="form-control" id="senha" name="senha" placeholder="Digite sua senha">
-        </div>
-        <button type="submit" class="btn btn-primary">Entrar</button>
-    </form>
+    <div class="container">
+
+        <form action="" method="get">
+            <div class="form-group">
+                <label for="email">Digite seu email</label>
+                <input type="text" class="form-control" id="email" name="email" placeholder="Digite seu email">
+            </div>
+            <div class="form-group">
+                <label for="email">Digite sua senha</label>
+                <input type="password" class="form-control" id="senha" name="senha" placeholder="Digite sua senha">
+            </div>
+            <button type="submit" class="btn btn-primary">Entrar</button>
+        </form>
+
+    </div>
+    
 
 
 </body>
